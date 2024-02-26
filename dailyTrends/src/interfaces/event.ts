8bb304bdafa6f042708080156d83a99bfbea0832
Rafa <raorcar3@gmail.com>
2024-02-27 00:21:10 +0100

    Remove dailyTrends

--- dailyTrends/src/interfaces/event.ts
+++
@@ -1,7 +0,0 @@
-export interface Event {
-  heading: string;
-  subHeading?: string;
-  link?: string;
-  date: string;
-  provider: string;
-}
--- dailyTrends/src/server.ts
+++
@@ -1,8 +0,0 @@
-import { App } from './app';
-import { ValidateEnv } from './utils/validateEnv';
-
-ValidateEnv();
-
-const server = new App();
-
-server.listen();
